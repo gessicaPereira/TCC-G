9a2c008e93cb2ae24782b8e2fc5bb4b436408e5d
gessicaPereira <gessica.pfs@gmail.com>
2023-10-08 19:43:48 -0300

    adjust

--- streamtest.py
+++ streamtest.py
@@ -18,7 +18,17 @@ import pandas as pd
 import plotly.express as px
 import io 
 
+
+##AJUSTAR ISSO É O TITULO DA PÁGINA 
+#st.set_page_config(
+  #  page_title="Pesquisa de Estabelecimentos",
+   # page_icon="🏢",
+    #layout="wide",
+#)
+
+
 df = pd.read_csv('estabcnaeok.csv')
+df_inativas = pd.read_csv('estab_inativos_ok.csv')
 
 
 #MENU QUE PRECISA DE AJUSTE
@@ -37,84 +47,121 @@ with st.sidebar:
 #logo = Image.open(r'C:\Users\08897\TCC\BASECOMPLETA\gatoum.jpg')
 #profile = Image.open(r'C:\Users\08897\TCC\BASECOMPLETA\gatodois.jpg')
 if choose == "Estabelecimentos": 
-    st.text('Testando 1')
 
-    #col1, col2 = st.columns( [0.8, 0.2])
-    #with col1:               # To display the header text using css style
-     #   st.markdown(""" <style> .font {
-      #      font-size:35px ; font-family: 'Cooper Black'; color: #FF9633;} 
-       #     </style> """, unsafe_allow_html=True)
-        #st.markdown('<p class="font">About the Creator</p>', unsafe_allow_html=True)    
-    #with col2:               # To display brand log
-     #   st.image(logo, width=130 )
+    col1, col2 = st.columns(2)
     
-    #st.write("Please visit My Data Talk's Medium blog at: https://medium.com/@insightsbees")    
-    #st.image(profile, width=700 )
-elif choose == "Localizações":
-    st.text('Testando 2')
-
-
-#MENU
-
-#
-#with st.sidebar: 
- #   selected = option_menu("Menu", ["Home Settings", "Teste"],
-  #                         icons=['house', 'gear'], menu_icon="cast", default_index=0)
-    
-   # if selected == "Teste":
-    #    st.write("Página de teste")
-    #else:
-     #   st.write("Outra página")
-
-  #  selected
-
-
-#COMEÇA A APLICAÇÃO ---->
-
-#df2 = pd.read_csv('estabelecimentos_total_dataok.csv')
-
-
-    #INICIO GRÁFICO 1 - PRA MIM GRÁFICO 1 ESTÁ OK
-df['DATA STC'] = pd.to_datetime(df['DATA STC'])
+        #INICIO GRÁFICO 1 - PRA MIM GRÁFICO 1 ESTÁ OK
+    df['DATA STC'] = pd.to_datetime(df['DATA STC'])
 
 # AQUI EU VOU ACESSAR A PROPIEDADE ANO DA DATA DE SITUAÇÃO CADASTRAL PARA AGRUPAR E CONTAR A PARTIR DO 
 # CNPJ O QUANTOS CNPJS CORRESPONDEM AQUELE ANO 
 # A VARIAVEL ARMAZENA OS ANOS COMO INDICE E A CONTAGEM DOS ESTABELECIMENTOS COM OS VALORES CORRESPONDENTES
-contagem_anos = df.groupby(df['DATA STC'].dt.year)['CNPJ O'].count().reset_index()
+    contagem_anos = df.groupby(df['DATA STC'].dt.year)['CNPJ O'].count().reset_index()
 
 # O GRÁFICO COM O EFEITO HOVER
-fig = go.Figure(data=go.Scatter(x=contagem_anos['DATA STC'], y=contagem_anos['CNPJ O'],
+    fig = go.Figure(data=go.Scatter(x=contagem_anos['DATA STC'], y=contagem_anos['CNPJ O'],
                                mode='lines+markers', hovertext=df['CNPJ O'],
                                hovertemplate='CNPJ: %{hovertext}<br>Quantidade: %{y}<extra></extra>'))
 
 # O LAYOUT DO GRÁFICO
-fig.update_layout(
-title='Número de estabelecimentos por ano',
-xaxis_title='Ano',
-yaxis_title='Quantidade de Estabelecimentos',
-title_x=0.28,
-title_font=dict(size=20)
-)
+    fig.update_layout(
+    title='Número de estabelecimentos por ano',
+    xaxis_title='Ano',
+    yaxis_title='Quantidade de Estabelecimentos',
+    title_x=0.28,
+    title_font=dict(size=20),
+    width= 750
+    )
+    st.plotly_chart(fig) 
+    #fim grafico 1
 
-st.plotly_chart(fig) 
-#FIM GRÁFICO 1
 
+    #INICIO GRÁFICO INATIVAS 
+    df_inativas['DATA STC'] = pd.to_datetime(df_inativas['DATA STC'])
 
-#GRAFICO 2 - NÃO FICOU MUITO BOM
-#contagem_atividades = df['CNAE PRINCP'].value_counts()
+    contagem_anos = df_inativas.groupby(df_inativas['DATA STC'].dt.year)['CNPJ O'].count().reset_index()
 
-#top_3_atividades = contagem_atividades.nlargest(3)
+    selected_years = st.slider("Selecione o intervalo de anos", int(contagem_anos['DATA STC'].min()), int(contagem_anos['DATA STC'].max()), (int(contagem_anos['DATA STC'].min()), int(contagem_anos['DATA STC'].max())))
+    fig = px.bar(contagem_anos, x='DATA STC', y='CNPJ O', title='Número de estabelecimentos inativos')
 
-#plt.rcParams['font.size'] = 12
+    fig.update_layout(
+    xaxis_title='Ano',
+    yaxis_title='Quantidade ',
+    title_x=0.30,
+    title_font=dict(size=20),
+    width=750,
+    barmode='stack'  # Essa opção define o modo de empilhamento das barras
+    )
 
-#plt.figure(figsize=(10, 20))
-#labels = [textwrap.fill(atividade, 15) for atividade in top_3_atividades.index]
-#plt.pie(top_3_atividades.values, labels=top_3_atividades.index, autopct='%1.1f%%', startangle=140)
-#plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-#plt.title('As 3 Atividades Econômicas Mais Frequentes na Cidade Específica')
-#plt.tight_layout()
+# Filtro dos dados baseado no intervalo de anos selecionado
+    filtered_data = contagem_anos[(contagem_anos['DATA STC'] >= selected_years[0]) & (contagem_anos['DATA STC'] <= selected_years[1])]
 
-#st.pyplot(plt)
+# Atualização do gráfico com os dados filtrados
+    fig.data[0].x = filtered_data['DATA STC']
+    fig.data[0].y = filtered_data['CNPJ O']
+
+# Exibição do gráfico com Streamlit
+    st.plotly_chart(fig) 
+    #fim grafico 1
+
+    #INICIO GRAFICO 2
+    contagem_atividades = df['CNAE PRINCP'].value_counts()
+    top_3_atividades = contagem_atividades.nlargest(3)
+
+    # Cria um DataFrame com os dados das atividades econômicas mais frequentes
+    top_3_df = pd.DataFrame({'Atividade': top_3_atividades.index, 'Frequência': top_3_atividades.values})
+
+    # Cria um gráfico de pizza interativo usando o Plotly Express
+    fig = px.pie(top_3_df, values='Frequência', names='Atividade')
+
+    fig.update_layout(
+    title='Três atividades econômicas mais presentes',
+    height=450,
+    title_font=dict(size=20),
+    title_x=0.25,
+    width=800, 
+    legend=dict(
+        orientation='h'))
+    st.plotly_chart(fig)
+
+    #INICIO GRAFICO 3 - inativos
+    contagem_atividades_inativas = df_inativas['CNAE PRINCP'].value_counts()
+    top_3_atividades_inativas = contagem_atividades_inativas.nlargest(3)
+
+    # Cria um DataFrame com os dados das atividades econômicas mais frequentes
+    top_3_df_inativas = pd.DataFrame({'Atividade': top_3_atividades_inativas.index, 'Frequência': top_3_atividades_inativas.values})
+
+    # Cria um gráfico de pizza interativo usando o Plotly Express
+    fig = px.pie(top_3_df_inativas, values='Frequência', names='Atividade')
+
+    fig.update_layout(
+    title='Atividades econômicas inativas',
+    height=450,
+    title_font=dict(size=20),
+    title_x=0.25,
+    width=800, 
+    legend=dict(
+        orientation='h'))
+    st.plotly_chart(fig)
+
+elif choose == "Localizações":
+    st.title('Pesquisa de Estabelecimentos')
+
+    name_local = st.text_input('Digite o nome do estabelecimento:')
+
+    filter_local = df[df['NOME FANT'].str.contains(name_local, case=False)]
+
+    if not filter_local.empty:
+        st.subheader('Informações do Estabelecimento')
+        st.write('Nome:', filter_local['NOME FANT'].iloc[0])
+        st.write('Data de Início da Atividade:', filter_local['DT IN ATV'].iloc[0])
+        st.write('Localização:', filter_local['LOGRD'].iloc[0])
+    else:
+        st.error('Nenhum estabelecimento encontrado com esse nome.')
+elif choose == "Sobre":
+    st.title('Aplicação TCC - nome')
+    st.markdown('Aplicação desenvolvida como trabalho de conclusão do curso bacharelado em Sistemas de Informação.')
+    st.markdown('Esaa interface foi desenvolvida com o intuito de auxiliar no processo decisório de novos empreendedores da cidade de Cedro no Ceará, bem como disponibilizar uma visão mais ampla acerca do empreendedorismo local.')
 
 
 
@@ -149,18 +196,19 @@ st.plotly_chart(fig)
 #fig.update_yaxes(title_text='Quantidade')
 #st.plotly_chart(fig)
 
+#ESSE É P GRAFICO D EDENSIDADE, LEGAL PORÉM MEIO CONFUSO
 #LEGAL ESSE, VERIFICAR DEPOIS SE ELE VAI FICAR 
 #GRAFICO 5
-fig = px.density_heatmap(df, x='DT IN ATV', title='Distribuição de Estabelecimentos')
-fig.update_xaxes(title_text='Ano de Início')
+#fig = px.density_heatmap(df, x='DT IN ATV', title='Distribuição de Estabelecimentos')
+#fig.update_xaxes(title_text='Ano de Início')
 
-fig.update_yaxes(title_text='Densidade')
+#fig.update_yaxes(title_text='Densidade')
 
-fig.update_layout(
-title_x=0.28,
-title_font=dict(size=20))
+#fig.update_layout(
+#title_x=0.28,
+#title_font=dict(size=20))
 
-st.plotly_chart(fig)
+#st.plotly_chart(fig)
 
 
 #with st.sidebar: 
@@ -180,27 +228,6 @@ st.plotly_chart(fig)
  #   st.write("Outra página")
 
 
-contagem_atividades = df['CNAE PRINCP'].value_counts()
-top_3_atividades = contagem_atividades.nlargest(3)
-
-# Cria um DataFrame com os dados das atividades econômicas mais frequentes
-top_3_df = pd.DataFrame({'Atividade': top_3_atividades.index, 'Frequência': top_3_atividades.values})
-
-# Cria um gráfico de pizza interativo usando o Plotly Express
-fig = px.pie(top_3_df, values='Frequência', names='Atividade')
-
-fig.update_layout(
-    title='Três atividades econômicas mais presentes',
-    height=450,
-    title_font=dict(size=20),
-    title_x=0.25,
-    width=800, 
-    legend=dict(
-        orientation='h'
-    )
-)
-
-st.plotly_chart(fig)
 
 #from geopy.geocoders import Nominatim
 #import folium
@@ -239,20 +266,48 @@ st.plotly_chart(fig)
 
 #st.markdown(m._repr_html_(), unsafe_allow_html=True)
 
+#teste pro grafico das localizações
+#import folium
+#import streamlit as st
 
-st.title('Pesquisa de Estabelecimentos')
+#from streamlit_folium import st_folium
 
-name_local = st.text_input('Digite o nome do estabelecimento:')
+# center on Liberty Bell, add marker
+#m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+#folium.Marker(
+ #   [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
+#).add_to(m)
 
-filter_local = df[df['NOME FANT'].str.contains(name_local, case=False)]
+# call to render Folium map in Streamlit
+#st_data = st_folium(m, width=725)
 
-if not filter_local.empty:
-    st.subheader('Informações do Estabelecimento')
-    st.write('Nome:', filter_local['NOME FANT'].iloc[0])
-    st.write('Data de Início da Atividade:', filter_local['DT IN ATV'].iloc[0])
-    st.write('Localização:', filter_local['LOGRD'].iloc[0])
-else:
-    st.error('Nenhum estabelecimento encontrado com esse nome.')
+
+
+# Página principal do Streamlit
+def main():
+    st.title("CNAES")
+
+    # Filtro por atividade
+    selected_activity = st.selectbox("Filtrar por atividade", df['CNAE PRINCP'].unique())
+
+    # Filtrar estabelecimentos ativos pela atividade selecionada
+    filtered_estabelecimentos = df[df['CNAE PRINCP'] == selected_activity]
+
+    st.subheader(f" {selected_activity}")
+    
+    table_data = filtered_estabelecimentos[['NOME FANT', 'BAIRRO']]
+    st.table(table_data)
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
 
 
 
